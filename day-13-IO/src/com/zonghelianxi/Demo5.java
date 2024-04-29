f407575223b64e17d4261719b9630252d81a9519
lixinan1234 <2489460735@qq.com>
2024-04-29 23:12:56 +0800

    随机抽取

---
+++ day-13-IO/src/com/zonghelianxi/Demo5.java
@@ -0,0 +1,13 @@
+package com.zonghelianxi;
+
+/**
+ * @author:lixinan
+ * @email:2489460735@qq.com
+ * @desc:
+ * @datetime: 2024/4/29 23:12
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+
+    }
+}
