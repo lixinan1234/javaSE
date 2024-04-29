981b3c8a24a852e0561f670c932c24fe2a3d5b11
lixinan1234 <2489460735@qq.com>
2024-04-29 23:06:38 +0800

    随机抽取

--- day-13-IO/src/com/Hutool/Demo1.java
+++ day-13-IO/src/com/Hutool/Demo1.java
@@ -1,5 +1,9 @@
 package com.Hutool;
 
+import cn.hutool.core.io.FileUtil;
+
+import java.util.List;
+
 /**
  * @author:lixinan
  * @email:2489460735@qq.com
@@ -8,6 +12,42 @@ package com.Hutool;
  */
 public class Demo1 {
     public static void main(String[] args) {
+         /*
+        FileUtil类:
+                file：根据参数创建一个file对象
+                touch：根据参数创建文件
+
+                writeLines：把集合中的数据写出到文件中，覆盖模式。
+                appendLines：把集合中的数据写出到文件中，续写模式。
+                readLines：指定字符编码，把文件中的数据，读到集合中。
+                readUtf8Lines：按照UTF-8的形式，把文件中的数据，读到集合中
+
+                copy：拷贝文件或者文件夹
+    */
+
+//         // file：根据参数创建一个file对象
+//        File xdy = FileUtil.file("C:\\Users\\李新安\\Desktop\\Java代码", "xdy","aaa", "a.txt");
+//        System.out.println(xdy);
+//
+//        //touch：根据参数创建文件
+//        File touch = FileUtil.touch(xdy);
+//        System.out.println(touch);
+//
+//        //writeLines：把集合中的数据写出到文件中，覆盖模式。
+//        ArrayList<String> list = new ArrayList<>();
+//        Collections.addAll(list,"aaa","bbb","ccc");
+//        File file = FileUtil.writeLines(list, "C:\\Users\\李新安\\Desktop\\Java代码\\b.txt", "UTF-8");
+//        System.out.println(file);
+//
+//        //appendLines：把集合中的数据写出到文件中，续写模式。
+//        ArrayList<String> list1 = new ArrayList<>();
+//        Collections.addAll(list1,"aaa","bbb","ccc");
+//        File file1 = FileUtil.appendLines(list1, "C:\\Users\\李新安\\Desktop\\Java代码\\b.txt", "UTF-8");
+//        System.out.println(file1);
+
+        // readLines：指定字符编码，把文件中的数据，读到集合中。
+        List<String> strings = FileUtil.readLines("C:\\Users\\李新安\\Desktop\\Java代码\\a.txt", "UTF-8");
+        System.out.println(strings);
 
     }
 }
---
+++ day-13-IO/src/com/zonghelianxi/Demo3.java
@@ -0,0 +1,55 @@
+package com.zonghelianxi;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * @author:lixinan
+ * @email:2489460735@qq.com
+ * @desc:
+ * @datetime: 2024/4/29 22:17
+ */
+public class Demo3 {
+    public static void main(String[] args) throws IOException {
+          /*需求：
+            需求：
+                有一个文件里面存储了班级同学的信息，每一个信息占一行。
+                格式为：张三-男-23
+                要求通过程序实现随机点名器。
+
+            运行效果：
+                第一次运行程序：随机同学姓名1（只显示名字）
+                第二次运行程序：随机同学姓名2（只显示名字）
+                第三次运行程序：随机同学姓名3（只显示名字）
+                …
+         */
+
+          //1.读取文件中学生的姓名
+        ArrayList list = new ArrayList();
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\李新安\\Desktop\\Java代码\\c.txt"));
+        String len;
+        while ((len = br.readLine())!=null){
+            list.add(len);
+        }
+        br.close();
+
+        //2.随机抽取(解法一)
+        Random r = new Random();
+        int index = r.nextInt(list.size());
+        String o = (String) list.get(index);
+        String[] split = o.split("-");
+        System.out.println(split[0]);
+
+        //2.随机抽取(解法二)
+        Collections.shuffle(list);
+        String o1 = (String) list.get(0);
+        String[] split1 = o1.split("-");
+        System.out.println(split1[0]);
+
+
+    }
+}
