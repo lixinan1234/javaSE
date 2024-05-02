3b852f88ab971b6a3ea3c16d7c2c4680ee5edb04
lixinan1234 <2489460735@qq.com>
2024-05-02 19:30:10 +0800

    登入注册小作业

---
+++ day-13-IO/src/com/zonghelianxi/dengruzhuce/Demo2.java
@@ -0,0 +1,13 @@
+package com.zonghelianxi.dengruzhuce;
+
+/**
+ * @author:lixinan
+ * @email:2489460735@qq.com
+ * @desc:
+ * @datetime: 2024/5/2 19:08
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+
+    }
+}
