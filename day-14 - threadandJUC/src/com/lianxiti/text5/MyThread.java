b6550fb059283ec73144915a630dc0e83c37ddd0
lixinan1234 <2489460735@qq.com>
2024-05-12 15:57:51 +0800

    多线程练习（抽奖箱抽奖）

---
+++ day-14 - threadandJUC/src/com/lianxiti/text5/MyThread.java
@@ -0,0 +1,43 @@
+package com.lianxiti.text5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author:lixinan
+ * @email:2489460735@qq.com
+ * @desc:
+ * @datetime: 2024/5/12 15:29
+ */
+public class MyThread extends Thread{
+
+    ArrayList<Integer> list;
+
+    public MyThread(ArrayList<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        //1.循环
+        while (true){
+            //2.同步代码块
+            synchronized (MyThread.class){
+                //3.判断共享数据是否到了末尾，(到了末尾)
+                if(list.size()==0){
+                    break;
+                }else {
+                    //3.判断共享数据是否到了末尾，(没有到末尾，执行核心逻辑)
+                    Collections.shuffle(list);
+                    Integer prize = list.remove(0);
+                    System.out.println(getName()+"产生了一个"+prize+"元大奖");
+                }
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
