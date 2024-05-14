93790978b45c7b08346d94c7beb707db0aab5ca0
lixinan1234 <2489460735@qq.com>
2024-05-14 23:37:49 +0800

    UDP协议

--- day-15-wangluo/src/com/UDP/fasongshuju/Demo1.java
+++ day-15-wangluo/src/com/UDP/fasongshuju/Demo1.java
@@ -13,7 +13,8 @@ public class Demo1 {
     public static void main(String[] args) throws IOException {
         //发送数据
 
-        //1.创建对象(快递公司)
+
+        //1.创建DatagramSocket对象(快递公司)
         //细节：
         //绑定端口，以后我们就是通过这个端口往外发送
         //空参：所有可用的端口中随机一个进行使用
@@ -21,11 +22,9 @@ public class Demo1 {
         DatagramSocket ds = new DatagramSocket();
 
         //2.打包数据
-        String str = "你好威啊！！";
+        String str = "你好威啊！！！";
         byte[] bytes = str.getBytes();
-
         InetAddress address = InetAddress.getByName("127.0.0.1");
-
         int port = 10086;
 
         DatagramPacket dp = new DatagramPacket(bytes,bytes.length,address,port);
@@ -33,8 +32,10 @@ public class Demo1 {
         //3.发送数据
         ds.send(dp);
 
+
         //4.释放资源
         ds.close();
+
 
 
     }
